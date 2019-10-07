eb21c4fc3660a79db0154bda8c9c4749618c4287
tsuchigumoo <35763044+tsuchigumoo@users.noreply.github.com>
2019-10-07 17:41:23 +0200

    Create load.py

---
+++ load.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from numpy import loadtxt
+from keras.models import load_model
+import numpy as np
+
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+
+img_rows, img_cols = 28, 28
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+# print('x_train shape:', x_train.shape)
+# print(x_train.shape[0], 'train samples')
+# print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+
+model = load_model('model.h5')
+
+x=(x_test[0])
+x=np.expand_dims(x, axis=0)
+prediction = model.predict(x)
+print(prediction)
+
+
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
+
